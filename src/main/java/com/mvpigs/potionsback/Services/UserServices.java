49ba4295bb79c5899f0d45dd74f930c897d3be52
Valenti <valendieguez89@gmail.com>
2019-01-15 18:06:35 +0100

    User services

--- src/main/java/com/mvpigs/potionsback/Services/UserServices.java
+++ src/main/java/com/mvpigs/potionsback/Services/UserServices.java
@@ -1,4 +1,20 @@
 package com.mvpigs.potionsback.Services;
 
+import com.mvpigs.potionsback.Classes.User;
+
 public class UserServices {
+
+    private static UserServices userServices;
+
+    public  static UserServices getConfigurador() {
+
+        if (userServices==null) {
+
+            userServices=new UserServices();
+        }
+        return userServices;
+    }
+
+
+
 }
