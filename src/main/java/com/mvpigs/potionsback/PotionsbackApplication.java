5f55fca8213219c196574ccdfbbc5c9ad15a17e1
Valenti <valendieguez89@gmail.com>
2019-01-15 17:22:15 +0100

    Initial commit

---
+++ src/main/java/com/mvpigs/potionsback/PotionsbackApplication.java
@@ -0,0 +1,14 @@
+package com.mvpigs.potionsback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PotionsbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PotionsbackApplication.class, args);
+    }
+
+}
+
